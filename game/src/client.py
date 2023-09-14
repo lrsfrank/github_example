24a26211a5e2e4e1b45439b5dfc657b4b615e8ad
qq805540531 <805540531@qq.com>
2023-09-14 17:52:39 +0800

    finish match_client

--- game/src/client.py
+++ game/src/client.py
@@ -7,8 +7,9 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+from sys import stdin
 
-def main():
+def operate(op, user_id, user_name, user_score):
     # Make socket
     transport = TSocket.TSocket('localhost', 9090)
 
@@ -27,10 +28,18 @@ def main():
 
     
     user = User(1, 'lrs', 1000)
-    client.add_user(user, "")
-
+    if op == "add":
+        client.add_user(user, "")
+    elif op == "remove":
+        client.remove_user(user, "")
     # Close!
     transport.close()
+
+def main():
+    for line in stdin:
+        op, user_id, user_name, user_score = line.split(" ")
+        operate(op, int(user_id), user_name, int(user_score))
+
 
 if __name__ == "__main__":
     main()
